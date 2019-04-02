73477f83f19de494c71c7e1b6e7ff30c0ee70213
Singh <asingh7@uspto.gov>
2019-04-02 17:13:27 -0400

    Initial commit

---
+++ src/main/java/io/avi/testapps/oauth/service/UserInfoOAuthService.java
@@ -0,0 +1,38 @@
+package io.avi.testapps.oauth.service;
+
+
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+@Service
+public class UserInfoOAuthService extends DefaultOAuth2UserService {
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(oAuth2UserRequest);
+
+        try {
+            return processOAuth2User(oAuth2UserRequest, oAuth2User);
+        } catch (AuthenticationException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            // Throwing an instance of AuthenticationException will trigger the OAuth2AuthenticationFailureHandler
+            throw new InternalAuthenticationServiceException(ex.getMessage(), ex.getCause());
+        }
+    }
+
+    private OAuth2User processOAuth2User(OAuth2UserRequest oAuth2UserRequest, OAuth2User oAuth2User) {
+        String email = (String) oAuth2User.getAttributes().get("email");
+        if (StringUtils.isEmpty(email)) {
+            throw new RuntimeException("Email not found from OAuth2 provider");
+        }
+
+        return oAuth2User;
+    }
+}
